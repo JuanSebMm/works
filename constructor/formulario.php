c3de1f3dbdf170fbc7bc48d64ad13601ee6bd743
JuanSebMm <juanmira716@gmail.com>
2021-09-18 23:36:12 -0500

    pruebas 1

---
+++ constructor/formulario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>persona</title>
+</head>
+<body>
+
+<form action="class.php" method="POST">
+
+<h1>Ingrese su nombre</h1><input name="inputs_1" type="text">
+
+<h1>Ingrese su ficha de formacion</h1><input name="inputs_3" type="text">
+
+<h1>Ingrese su edad</h1><input name="inputs_2" type="text">
+
+<input type="submit" placeholder="enviar">
+
+
+</form>
+
+    
+</body>
+</html>

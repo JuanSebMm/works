b3c88ea8b4d712fe5fab6021eeb70b6716ae321b
JuanSebMm <88688082+JuanSebMm@users.noreply.github.com>
2021-09-27 15:59:21 -0500

    reemplazo de suma mal

--- proyecto/js/suma.js
+++ proyecto/js/suma.js
@@ -1,12 +1,14 @@
-
-function sumaDeLosParesDel0Al(cont){
-    var suma = 0
-    for (var i=1; i<cont.length; i++){
-      if (cont[i]%2 == 0) {
-        suma += i
-      }
+var SumaPar=(vec)=>{
+  suma=0;
+  for (let i = 0; i < vec.length; i++) {
+    if (vec[i]%2==0) {
+      suma +=vec[i]      
     }
-    return suma
+    
   }
-  
-console.log(sumaDeLosParesDel0Al(cont));
+  return suma;
+}
+
+n=[2,3,4,5,8];
+
+console.log(SumaPar(n));

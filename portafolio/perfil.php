08184f9b476548a8530a28d2ec1f29c61cbdd117
JuanSebMm <juanmira716@gmail.com>
2021-09-30 11:58:22 -0500

    trabajo de perfil

---
+++ portafolio/perfil.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links de estilos -->
+
+    <link rel="stylesheet" href="css/cuerpo.css">  <!-- Estilos del cuerpo   -->
+    <link rel="stylesheet" href="css/menu.css">  <!-- Estilos del menu Lat -->
+    <link rel="stylesheet" href="">  <!-- Estilos del Portafolio -->
+
+    <!-- ---------------- -->
+    <title>Portafolio || Especialidades web</title>
+</head>
+<body>
+    <!-- Menu   lateral -->
+    <div id="sidemenu" class="menu-collapsed">
+        <div id="header">
+            <div id="tittle"><span>Portafolio ADSI</span></div>
+            <div id="menu-btn">
+                <div class="btn-hamburguer">Herramientas</div>
+                <div class="btn-hamburguer"></div>
+                <div class="btn-hamburguer"></div>
+            </div>
+        </div>
+    <!-- -------------- -->
+
+    <!-- Info  Personal -->
+        <div id="profile">
+            <div id="photo"><img src="" alt=""></div>
+            <div id="name"><span>Juan sebastian Miranda</span></div>
+        </div>
+
+
+        <!-- Especialidades -->
+        <div id="menu-items">
+            <div class="items">
+                <a href="">
+                    <div class="icon"><i class="fas fa-cogs"></i></div>
+                    <div class="tittle"><span>Herramientas</span></div>
+                </a>
+            </div>
+        </div>
+    <!-- -------------- -->
+
+    </div>
+
+    <div id="main-container">
+        Hola a todos
+    </div>
+    
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/js/all.min.js"></script>
+</html>

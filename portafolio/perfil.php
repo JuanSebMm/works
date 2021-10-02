b3e48c844e793f91cf1c173d1a9a5cb2b3755e85
Juan Miranda <juanmira716@gmail.com>
2021-10-02 13:47:58 -0500

    Actualizacion de portafolio

--- portafolio/perfil.php
+++ portafolio/perfil.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Links de estilos -->
 
-    <link rel="stylesheet" href="css/corp.css">  <!-- Estilos del cuerpo   -->
-    <link rel="stylesheet" href="css/main.css">  <!-- Estilos del menu Lat -->
+    <link rel="stylesheet" href="css/cuerpo.css">  <!-- Estilos del cuerpo   -->
+    <link rel="stylesheet" href="css/menu.css">  <!-- Estilos del menu Lat -->
     <link rel="stylesheet" href=""> <!-- Estilos del Portafolio -->
 
     <!-- ---------------- -->
@@ -64,9 +64,49 @@
 
     </div>
 
-    <div id="main-container">
-        Hola a todos
+    <div class="wrapper">
+            <!-- Cuerpo del proyecto -->
+    <main class="main">
+        <div class="info-content">
+            <h1>Soy<span> Juan</span> Sebastian <span>Miranda</span></h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta error ex, voluptatum similique quam ipsum libero provident maiores quaerat debitis quibusdam dolor asperiores iure distinctio enim facilis suscipit expedita voluptatem!</p>
+            <a dowload href="">
+                <button class="dowload-cv">Dowload CV</button>
+            </a>
+        </div>
+        
+        <div><img src="img/construir.png" alt="Imagen de codigo"></div>
+
+        
+    </main>
+
+
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-22.29,-7.38 C4.80,354.78 209.65,-41.92 533.57,81.42 L500.27,-13.30 L0.00,0.00 Z" style="stroke: none; fill: #191c32;"></path></svg></div>
+
+
+    <div class="about">
+        <img src="img/Sobmi.png" alt="">
+        <div class="about-info">
+            <h2>Sobre me</h2>
+            <div class="divider"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error facilis cupiditate placeat? Ut consectetur iusto similique fugiat ipsum facilis vel quam molestias, itaque ad, nobis dolorem laudantium. Repudiandae, earum odit.</p>
+            <p>Obcaecati dolorum, iure ad eius voluptatibus ratione esse vel pariatur consectetur voluptas animi error maiores itaque eos suscipit repellendus dolores, minus quod veritatis facere delectus placeat ex libero quaerat. Quod.</p>
+
+            <button class="dowload-cv">Mas informacion</button>
+        </div>
     </div>
+
+
+    
+
+    </div>
+
+
+
+    
+
+
+
     
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/js/all.min.js">

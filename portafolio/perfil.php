d05d1382c0c5da9e9ca46af0d9b19278972ab2c4
JuanSebMm <juanmira716@gmail.com>
2021-10-03 00:21:42 -0500

    expancion de portafolio

--- portafolio/perfil.php
+++ portafolio/perfil.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="css/cuerpo.css">  <!-- Estilos del cuerpo   -->
     <link rel="stylesheet" href="css/menu.css">  <!-- Estilos del menu Lat -->
-    <link rel="stylesheet" href=""> <!-- Estilos del Portafolio -->
+    <link rel="stylesheet" href="css/galBar.css"> <!-- Estilos del Portafolio -->
 
     <!-- ---------------- -->
     <title>Portafolio || Especialidades web</title>
@@ -24,17 +24,17 @@
                 <div class="btn-hamburguer"></div>
             </div>
         </div>
-    <!-- -------------- -->
+     <!-- -------------- -->
 
-    <!-- Info  Personal -->
-        <div id="profile">
+     <!-- Info  Personal -->
+         <div id="profile">
             <div id="photo"><img class="imagen" src="img/mipersona.jpg" alt=""></div>
             <div id="name"><span>Juan sebastian Miranda</span></div>
-        </div>
+         </div>
 
 
-        <!-- Especialidades -->
-        <div id="menu-items">
+     <!-- Especialidades -->
+         <div id="menu-items">
             <div class="item">
                 <a href="">
                     <div class="icon"><img src="img/engranaje.png" alt=""></div>
@@ -59,14 +59,18 @@
                     <div class="tittle"><span>Herramientas</span></div>
                 </a>
             </div>
-        </div>
-    <!-- -------------- -->
+         </div>
+     <!-- -------------- -->
 
     </div>
 
     <div class="wrapper">
+
+        <!-- ------- -->
             <!-- Cuerpo del proyecto -->
-    <main class="main">
+        <!-- ------- -->
+
+         <main class="main">
         <div class="info-content">
             <h1>Soy<span> Juan</span> Sebastian <span>Miranda</span></h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta error ex, voluptatum similique quam ipsum libero provident maiores quaerat debitis quibusdam dolor asperiores iure distinctio enim facilis suscipit expedita voluptatem!</p>
@@ -78,36 +82,75 @@
         <div><img src="img/construir.png" alt="Imagen de codigo"></div>
 
         
-    </main>
+         </main>
 
 
-    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-22.29,-7.38 C4.80,354.78 209.65,-41.92 533.57,81.42 L500.27,-13.30 L0.00,0.00 Z" style="stroke: none; fill: #191c32;"></path></svg></div>
+      <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-22.29,-7.38 C4.80,354.78 209.65,-41.92 533.57,81.42 L500.27,-13.30 L0.00,0.00 Z" style="stroke: none; fill: #191c32;"></path></svg></div>
 
 
-    <div class="about">
-        <img src="img/Sobmi.png" alt="">
-        <div class="about-info">
-            <h2>Sobre me</h2>
-            <div class="divider"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error facilis cupiditate placeat? Ut consectetur iusto similique fugiat ipsum facilis vel quam molestias, itaque ad, nobis dolorem laudantium. Repudiandae, earum odit.</p>
-            <p>Obcaecati dolorum, iure ad eius voluptatibus ratione esse vel pariatur consectetur voluptas animi error maiores itaque eos suscipit repellendus dolores, minus quod veritatis facere delectus placeat ex libero quaerat. Quod.</p>
+     <div class="about  contenido-textos">
+         <img src="img/Sobmi.png" alt="">
+         <div class="about-info">
+         <h3><span>1</span>Los mejores productos</h3>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt veniam eius aspernatur ad
+                         consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto animi  pariatur,
+                         velit doloribus laboriosam ut.</p>
+                     <h3><span>2</span>Los mejores productos</h3>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt veniam eius aspernatur ad
+                         consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto animi pariatur,
+                         velit doloribus laboriosam ut.</p>
 
-            <button class="dowload-cv">Mas informacion</button>
-        </div>
-    </div>
-
-
-    
-
-    </div>
-
+             <button class="dowload-cv">Mas informacion</button>
+         </div>
+     </div>
 
 
-    
+     <div class="Gal-pro">
+         
+      <section class="about-services">
+      <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-4.79,81.42 C209.08,-70.53 380.07,183.06 505.92,62.67 L500.00,0.00 L0.00,0.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
 
+            <div class="contenedor">
+                <h2 class="titulo">Lenguajes De<span> Programacion</span></h2>
+                <div class="servicio-cont">
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                    <div class="servicio-ind">
+                        <img src="img/engranaje.png" alt="">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, qui?</p>
+                    </div>
+                </div>
+            </div>
+      </section>
 
+     </div>
+     
 
-    
+    </div>    
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/js/all.min.js">
 </script>

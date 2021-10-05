59d62eb61412c1350faf3ea2acffb6449c419265
Juan Miranda <juanmira716@gmail.com>
2021-10-05 13:56:47 -0500

    crud rapido

---
+++ ManualUser/crudRp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    	<h1>¡Suscribete!</h1>
+    	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="email" name="email" placeholder="Email">
+    	<input type="submit" name="register">
+    </form>
+
+    <?php
+    include("registro.php"); 
+    ?>
+    
+</body>
+</html>
